a4d41aeca5ec2751a1b242682c8c03425b4c6345
mengf1 <meng.fang@unimelb.edu.au>
2019-01-03 14:26:00 +0800

    Update

---
+++ dher/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dher',
+    version='0.0.1',
+    description='The implementation of Dynamic Hindsight Experience Replay',
+    author='Bei Shi, Meng Fang',
+    author_email='aiphy@tencent.com',
+    install_requires=['gym', 'numpy', 'baselines'])
