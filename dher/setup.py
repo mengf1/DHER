19813044fecab49f3582cf8c0b86854be3056e33
mengf1 <meng.fang@unimelb.edu.au>
2019-01-03 21:46:27 +0800

    Update

--- dher/setup.py
+++ dher/setup.py
@@ -4,6 +4,6 @@ setup(
     name='dher',
     version='0.0.1',
     description='The implementation of Dynamic Hindsight Experience Replay',
-    author='Bei Shi, Meng Fang',
+    author='Meng Fang, Bei Shi',
     author_email='aiphy@tencent.com',
     install_requires=['gym', 'numpy', 'baselines'])
--- dygym/setup.py
+++ dygym/setup.py
@@ -4,6 +4,6 @@ setup(
     name='dygym',
     version='0.0.1',
     description='The dynamic goal gym.',
-    author='Cheng Zhou, Meng Fang',
+    author='Cheng Zhou, Bei Shi, Meng Fang',
     author_email='aiphy@tencent.com',
     install_requires=['gym', 'numpy', 'mujoco_py', 'pygame'])
