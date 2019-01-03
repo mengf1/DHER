2ca46aa23bccab8d04887387e1996434b2626d13
mengf1 <meng.fang@unimelb.edu.au>
2019-01-03 14:25:45 +0800

    Update

--- dygym/setup.py
+++ dygym/setup.py
@@ -4,6 +4,6 @@ setup(
     name='dygym',
     version='0.0.1',
     description='The dynamic goal gym.',
-    author='Meng Fang, Cheng Zhou, Bei Shi',
+    author='Cheng Zhou, Meng Fang',
     author_email='aiphy@tencent.com',
     install_requires=['gym', 'numpy', 'mujoco_py', 'pygame'])
